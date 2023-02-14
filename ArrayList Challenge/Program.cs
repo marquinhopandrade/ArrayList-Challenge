953ca8b6e05239d464a4e1d5cac127f6580656df
Marcos Andrade <mvpdeandrade@hotmail.com>
2023-02-14 17:57:32 -0300

    Adicionar arquivos de projeto.

---
+++ ArrayList Challenge/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArrayList_Challenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Numbers = new List<int>(71);
+            Console.WriteLine("numbers.count: {0}", Numbers.Count);
+            for (int i = 100, j = 0; i < 171;j++, i++)
+            {
+                int number = i;
+                Numbers.Add(number);
+
+            }
+        }
+    }
+}
